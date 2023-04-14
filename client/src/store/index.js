6da83b8f8eb2b46043c7ffd38a5395c89276bc14
Wilfred Marete <marete015@gmail.com>
2023-04-14 12:43:47 +0300

    first commit

---
+++ client/src/store/index.js
@@ -0,0 +1,12 @@
+import { proxy } from 'valtio';
+
+const state = proxy({
+    intro: true,
+    color: '#EFBD48',
+    isLogoTexture: true,
+    isFullTexture: false,
+    logoDecal: './11.jpg',
+    fullDecal: './11.jpg'
+});
+
+export default state;
